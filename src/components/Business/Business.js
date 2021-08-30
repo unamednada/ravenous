8128ccfa5138ae114648dde238e62d6d190ca9ae
unamednada <dias.gustavo.dias@gmail.com>
2021-08-29 22:33:10 -0300

    Add export default business

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './Business.css';
 
 const business = {
   imageSrc: "https://content.codecademy.com/programs/react/ravenous/pizza.jpg",
@@ -39,3 +40,5 @@ class Business extends React.Component {
     );
   }
 }
+
+export default Business;
