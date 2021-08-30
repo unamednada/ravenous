25d52ae94350b48c19066c4419924902ccdb8e6b
unamednada <dias.gustavo.dias@gmail.com>
2021-08-30 09:31:16 -0300

    Create searchbar component and rendersortoptions method

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,8 +2,16 @@ import React from 'react';
 import './SearchBar.css';
 
 const sortByOptions = {
-  'Best Match': '',
-  'Highest Rated': '',
-  'Most Reviewed': ''
+  'Best Match': 'best_match',
+  'Highest Rated': 'rating',
+  'Most Reviewed': 'review_count'
 }
 
+class SearchBar extends React.Component {
+  
+  renderSOrtByOptions() {
+    return Object.keys(sortByOptions).map((sortByOption) => {
+      const sortByOptionValue = sortByOptions[sortByOption];
+      return <li key={sortByOptionValue}>{sortByOption}</li>
+    })
+}
