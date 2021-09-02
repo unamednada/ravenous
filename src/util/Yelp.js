4352aa925b2bb5eaaf5537596f125cd2536d1e26
unamednada <dias.gustavo.dias@gmail.com>
2021-09-02 10:27:25 -0300

    Complete Yelp module

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,3 +1,31 @@
 const ID = 'G6ChgkSQjjwli4uzDJc6lA';
 const apiKey = 'WhorSdrpjQgt8Oax8fA9wGYNh0AUeVqzoN8B8reMBUcuoy6JL72mPlMTSawyjf4AjKE-IYAyj8o7ZU1_cUMvhP5LHEBJ0EVktfrbnHYbbYUDhQL7EvKHqyRpNM0wYXYx';
 
+const Yelp = {
+  search(term, location, sortBy) {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      }
+    })
+    .then((response) => response.json())
+    .then((jsonResponse) => {
+      if (jsonResponse.businesses) {
+        jsonResponse.businesses.map(({ id, image_url, name, location, categories, rating, review_count }) => ({
+          id,
+          imageSrc: image_url,
+          name,
+          address: location.address1,
+          city: location.city,
+          state: location.state,
+          zipCode: location.zip_code,
+          category: categories[0].title,
+          rating: rating,
+          reviewCount: review_count,
+        }))
+      }
+    })
+  }
+}
+
+export default Yelp;
